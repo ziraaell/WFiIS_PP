99ac709db72a664aaa04d1f2530fac2d7252ff94
Helen <122322678+ziraaell@users.noreply.github.com>
2023-02-28 01:03:46 +0100

    Update zad2.c

--- lab1/zad2.c
+++ lab1/zad2.c
@@ -12,7 +12,6 @@ void odwroc (int *tab, int tab_size)
 {
     for(int i = 0; i < (tab_size-1)/2; i++)
         zamiana(tab+i, tab+tab_size-1-i); 
-
 }
 
 void wymien_tablice (int *tab1, int *tab2, int tab1_size, int tab2_size)
@@ -29,7 +28,7 @@ int main()
 
     zamiana(&a,&b);
 
-    printf ("PO ZAMIANIE: a = %d, b = %d\nb)\nTABLICA PRZED ZAMIANA: ", a, b);
+    printf ("PO ZAMIANIE: a = %d, b = %d\nb)\nTABLICA PRZED ODWROCENIEM:\n", a, b);
 
     int tab [6] = {1, 2, 3, 4, 5, 6}; // b) przykladowa tablica
     int tab_size = sizeof(tab) / sizeof(int);
@@ -37,13 +36,13 @@ int main()
     for(int i = 0; i < tab_size; i++)
     printf("%d ", *(tab+i));
 
-    printf("\nTABLICA PO ZAMIANIE:   ");
+    printf("\nTABLICA PO ODWROCENIU:\n");
     odwroc(tab, tab_size);
 
     for(int i = 0; i < tab_size; i++)
     printf("%d ", *(tab+i));
 
-    printf("\nc)\nTABLICE PRZED WYMIANA\ntablica 1: ");
+    printf("\nc)\nTABLICE PRZED WYMIANA:\ntablica 1: ");
 
     int tab1[10] = {1 , 13, 15, 16, 17, 19, 20, 1, 9, 0};   //c) przykladowe tablice
     int tab2[5] = {8, 50, 100, 3, 5};
@@ -53,11 +52,13 @@ int main()
 
     for(int i = 0; i < tab1_size; i++)
     printf("%d ", *(tab1+i));
+    
     printf("\ntablica 2: ");
+    
     for(int i = 0; i < tab2_size; i++)
     printf("%d ", *(tab2+i));
 
-    printf("\nTABLICE PO WYMIANIE\ntablica 1: ");
+    printf("\nTABLICE PO WYMIANIE:\ntablica 1: ");
     wymien_tablice(tab1, tab2, tab1_size, tab2_size);
 
     for(int i = 0; i < tab1_size; i++)
